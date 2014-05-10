b73505b928fc0475e8d1e4bd40deffee6431ac07
Luke <lukeshi808@gmail.com>
2014-05-09 21:26:19 -0700

    Testing
    test

---
+++ WolongTests/WolongTests.m
@@ -0,0 +1,34 @@
+//
+//  WolongTests.m
+//  WolongTests
+//
+//  Created by Luke Shi on 4/3/14.
+//  Copyright (c) 2014 Luke Shi. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WolongTests : XCTestCase
+
+@end
+
+@implementation WolongTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
